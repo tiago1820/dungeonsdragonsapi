d46617c94ee0d6e56dda4a59512f42a8016b003f
tiago <tiago.zdo@gmail.com>
2024-04-23 20:38:49 -0300

    🇧🇷 1- Defini as principais models do meu projeto, incluindo User, Character, Episode e Location.    2- Criei um diagrama simples do banco de dados para visualizar a estrutura do meu projeto.
    🇪🇸 1- Definí los modelos principales de mi proyecto, incluyendo Usuario, Personaje, Episodio y Ubicación.
       2- Creé un diagrama simple de la base de datos para visualizar la estructura de mi proyecto.

--- src/db.js
+++ src/db.js
@@ -1,9 +1,46 @@
 import { Sequelize } from "sequelize";
-import { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME } from "./constants/index.js";
+import {
+    DB_USER,
+    DB_PASSWORD,
+    DB_HOST,
+    DB_PORT,
+    DB_NAME
+} from "./constants/index.js";
+
+import UserModel from "./models/user.model.js";
+import CharacterModel from "./models/character.model.js";
+import EpisodeModel from "./models/episode.model.js";
+import LocationModel from "./models/location.model.js";
+import CharEpisodeModel from "./models/charEpisode.model.js";
+import CharLocationModel from "./models/charLocation.model.js";
 
 const sequelize = new Sequelize(
     `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`,
     { logging: false, native: false }
 );
 
-export { sequelize as conn };
+UserModel(sequelize);
+CharacterModel(sequelize);
+EpisodeModel(sequelize);
+LocationModel(sequelize);
+CharEpisodeModel(sequelize);
+CharLocationModel(sequelize);
+
+const {
+    User,
+    Character,
+    Episode,
+    Location,
+    CharEpisode,
+    CharLocation
+} = sequelize.models;
+
+export {
+    User,
+    Character,
+    Episode,
+    Location,
+    CharEpisode,
+    CharLocation,
+    sequelize as conn
+};
