b2e84511a4597728fd44d01cb1e5093e464bb0ec
tiago <tiago.zdo@gmail.com>
2024-04-26 20:39:30 -0300

    1- Fue creado el metodo editUser responsable por editar los datos del usuario. 2- Fue creado la Clase Encryption, que es un util con el metodo encryptPassword, el cual hace la encriptación de un password recibido, usando la libreria bcryptjs.

---
+++ src/utils/encryption.util.js
@@ -0,0 +1,9 @@
+import bcrypt from "bcryptjs";
+
+export class Encryption {
+
+    encryptPassword = async (password) => {
+        const salt = await bcrypt.genSalt(10);
+        return bcrypt.hash(password, salt);
+    };
+}
