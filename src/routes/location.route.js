ea59b50c42e3442da93676c5d4a36539a2eae76a
tiago <tiago.zdo@gmail.com>
2024-05-08 07:30:06 -0300

    feat: Added getLocationByIds and editLocation methods in LocationController.

--- src/routes/location.route.js
+++ src/routes/location.route.js
@@ -7,3 +7,5 @@ const auth = new Authenticate();
 const location = new LocationController();
 locationRouter.post("/location/create", auth.authenticate, location.createLocation);
 locationRouter.get("/location", auth.authenticate, location.getAllLocation);
+locationRouter.get("/location/:ids", auth.authenticate, location.getLocationByIds);
+locationRouter.put("/location/:id", auth.authenticate, location.editLocation);
