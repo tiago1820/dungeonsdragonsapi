527eee329a47c8149b957f3f2e4e304ac1dd8c0d
tiago <tiago.zdo@gmail.com>
2024-05-08 06:46:45 -0300

    feat: Created LocationController class

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,9 +5,11 @@ import { authRoute } from "./auth.route.js";
 import { userRoute } from "./user.routes.js";
 import { characterRouter } from "./character.router.js";
 import { episodeRouter } from "./episode.route.js";
+import { locationRouter } from "./location.route.js";
 
 router.use(apiRoute);
 router.use(authRoute);
 router.use(userRoute);
 router.use(characterRouter);
 router.use(episodeRouter);
+router.use(locationRouter);
